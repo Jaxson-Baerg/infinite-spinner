7e4591236c483f9f6816be07135b2c4edd5e61b5
Jaxson-Baerg <jaxson.baerg@gmail.com>
2022-09-27 14:46:52 +0000

    refactored to include a callback to stop as well as a newline after stopped

--- spinner1.js
+++ spinner1.js
@@ -1,7 +1,17 @@
-const animations = ["|", "/", "-", "\\"];
-let i = 0;
 
-const spinner = setInterval(() => {
-  process.stdout.write(`\r${animations[i]}`);
-  i < 3 ? i++ : i = 0;
-}, 120);
+let spinner;
+const infiniteSpinner = () => {
+  const animations = ["|", "/", "-", "\\"];
+  let i = 0;
+
+  spinner = setInterval(() => {
+    process.stdout.write(`\r${animations[i]}`);
+    i < 3 ? i++ : i = 0;
+  }, 120);
+}
+
+infiniteSpinner();
+setTimeout(() => {
+  clearInterval(spinner);
+  console.log();
+}, 10000);
